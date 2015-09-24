ed85b9d5b5375450975ad46e885d78e34d75d6b0
magicwifi <huangzhe1218@163.com>
2015-09-24 18:40:27 +0800

    4st

---
+++ readfifo/src/utils.c
@@ -0,0 +1,130 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <uci.h>  
+#include <unistd.h>  
+
+#if defined(__NetBSD__)
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+#include <utils.h>
+#endif
+
+#ifdef __linux__
+#include <netinet/in.h>
+#include <net/if.h>
+#endif
+
+#include <string.h>
+#include <pthread.h>
+#include <netdb.h>
+#include "utils.h"
+
+
+
+char * ssidRead() {
+
+  struct uci_context *c;
+  struct uci_ptr p;
+
+  char *ssid = safe_strdup ("wireless.@wifi-iface[0].ssid"); 
+  c = uci_alloc_context();
+  if (uci_lookup_ptr (c, &p, ssid, true) != UCI_OK)
+    {
+      uci_perror (c, "XXX");
+      return 1;
+    }
+
+  char *str = strdup(p.o->v.string);
+  uci_free_context (c);
+  free(ssid);
+  return str; 
+}
+
+char *
+get_iface_mac(const char *ifname)
+{
+#if defined(__linux__)
+	int r, s;
+	struct ifreq ifr;
+	char *hwaddr, mac[13];
+
+	strcpy(ifr.ifr_name, ifname);
+
+	s = socket(PF_INET, SOCK_DGRAM, 0);
+	if (-1 == s) {
+		printf("get_iface_mac socket: %s\n", strerror(errno));
+		return NULL;
+	}
+
+	r = ioctl(s, SIOCGIFHWADDR, &ifr);
+	if (r == -1) {
+		printf("get_iface_mac ioctl(SIOCGIFHWADDR): %s\n", strerror(errno));
+		close(s);
+		return NULL;
+	}
+
+	hwaddr = ifr.ifr_hwaddr.sa_data;
+	close(s);
+	snprintf(mac, sizeof(mac), "%02X%02X%02X%02X%02X%02X", 
+			hwaddr[0] & 0xFF,
+			hwaddr[1] & 0xFF,
+			hwaddr[2] & 0xFF,
+			hwaddr[3] & 0xFF,
+			hwaddr[4] & 0xFF,
+			hwaddr[5] & 0xFF
+		);
+
+	return safe_strdup(mac);
+#elif defined(__NetBSD__)
+	struct ifaddrs *ifa, *ifap;
+	const char *hwaddr;
+	char mac[13], *str = NULL;
+	struct sockaddr_dl *sdl;
+
+	if (getifaddrs(&ifap) == -1) {
+		printf("getifaddrs(): %s\n", strerror(errno));
+		return NULL;
+	}
+	for (ifa = ifap; ifa != NULL; ifa = ifa->ifa_next) {
+		if (strcmp(ifa->ifa_name, ifname) == 0 &&
+				ifa->ifa_addr->sa_family == AF_LINK)
+			break;
+	}
+	if (ifa == NULL) {
+		printf("%s\n: no link-layer address assigned");
+		goto out;
+	}
+	sdl = (struct sockaddr_dl *)ifa->ifa_addr;
+	hwaddr = LLADDR(sdl);
+	snprintf(mac, sizeof(mac), "%02X%02X%02X%02X%02X%02X",
+			hwaddr[0] & 0xFF, hwaddr[1] & 0xFF,
+			hwaddr[2] & 0xFF, hwaddr[3] & 0xFF,
+			hwaddr[4] & 0xFF, hwaddr[5] & 0xFF);
+
+	str = safe_strdup(mac);
+out:
+	freeifaddrs(ifap);
+	return str;
+#else
+	return NULL;
+#endif
+}
+
+
+
+
+
+
---
+++ readfifo/src/utils.h
@@ -0,0 +1,6 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+char * ssidRead(); 
+char *get_iface_mac(const char *ifname);
+
+#endif /* _UTIL_H_ */
